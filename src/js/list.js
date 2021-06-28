1be8824849c8176f58a1f415c1b40d297f5c7fab
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-06-28 18:30:47 +0200

    Добавляет js-файл

---
+++ src/js/list.js
@@ -0,0 +1,59 @@
+import card from '../templates/card.hbs';
+import menuSet from '../menu.json';
+
+const menuItems = document.querySelector('.js-menu');
+
+menuItems.insertAdjacentHTML('beforeend', card(menuSet));
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+const bodyTheme = document.querySelector('body');
+const inputRef = document.querySelector("#theme-switch-toggle");
+
+inputRef.addEventListener('change', checkboxValue);
+
+function checkboxValue (e) {
+    const checkboxCurrent = e.currentTarget.checked;
+    
+    if (checkboxCurrent === true) {
+        checkedTrue()
+        console.log("+");
+    } else {
+        checkedFalse()
+        console.log('-');
+    }
+}
+
+function checkedTrue() {
+    bodyTheme.classList.add(Theme.DARK);
+    bodyTheme.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+    inputRef.checked = true;
+}
+
+function checkedFalse() {
+    bodyTheme.classList.add(Theme.LIGHT);
+    bodyTheme.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+    inputRef.checked = false;
+}
+
+function defoltOrMyTheme() {
+
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme === Theme.LIGHT || myTheme === null) {
+        checkedFalse();
+        return;
+    }
+
+    if (currentTheme === Theme.DARK) {
+        checkedTrue();
+        return;
+    }
+}
+
+defoltOrMyTheme();
